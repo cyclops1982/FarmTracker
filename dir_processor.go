6d3e193909167d3a55d6ff0053b9887ac5a827ff
Ruben d'Arco <cyclops@prof-x.net>
2021-01-04 14:06:17 +0000

    Send data on a TCP connection.
    Has:
    - Validation of JSON data
    - Simple filtering (we should do this based on event)
    
    The concept here works, and we can receive it via telnet as a client. All works as expected
    I'm sure there's some issues with the sockets, but for now it seems to work as expected.
    
    The filtering used here is very simple, and i believe that itmight actually be nice to create a program that does the same, but pushes the mesages to MQTT or something like that. Because you'd then be able to filter it based on the client's subscription.

--- dir_processor.go
+++ dir_processor.go
@@ -6,6 +6,10 @@ import (
 	"path/filepath"
 	"os"
 	"time"
+	"net"
+	"fmt"
+	"encoding/json"
+	"io/ioutil"
 )
 
 func FindFiles(ch chan string, path string) {
@@ -30,16 +34,60 @@ func FindFiles(ch chan string, path string) {
 }
 
 
-var inputdir string
-func main() {
-	flag.StringVar(&inputdir, "inputdir", "dumps/", "The directory to process.")
-	flag.Parse()
-
+func HandleClient(con net.Conn) {
+	defer con.Close()
 	ch := make(chan string)
 	go FindFiles(ch, inputdir)
 
 	for file := range ch {
-		log.Println("Processing ", file)
+		bytes, err := ioutil.ReadFile(file)
+		if err != nil {
+			log.Println("Failed to read ", file, ". Error:", err)
+			continue
+		}
+		// Verify that it's valid JSON.
+		// Possibly we'd do some filtering too.
+		var jsonData interface{}
+		err = json.Unmarshal(bytes, &jsonData)
+		if err != nil {
+			log.Println("Failed to parse JSON in ",file,". Error: ", err)
+			continue
+		}
+		realData := jsonData.(map[string]interface{})
+		if realData["applicationID"] != "1" {
+			log.Println("JSON payload is not for application 1. Skipping.")
+			continue
+		}
+		writenbytes, err := con.Write(bytes)
+		if err != nil {
+			log.Println("Failed to write bytes: ", err)
+		} else {
+			log.Println("Wrote ",writenbytes," to ", con.RemoteAddr())
+		}
+	}
+}
+
+
+var inputdir string
+func main() {
+	flag.StringVar(&inputdir, "inputdir", "dumps/", "The directory to process.")
+	var tcpPort = flag.Int("port", 29000, "The port to listen on. Default is 29000.")
+	var ipAddress = flag.String("address", "0.0.0.0", "The IP address to bind to, default is 0.0.0.0.")
+	flag.Parse()
+
+	sock, err := net.Listen("tcp", fmt.Sprintf("%s:%d", *ipAddress, *tcpPort))
+	if err != nil {
+		log.Fatal("Couldn't open TCP socket.", err)
+	}
+
+	// Wait for clients to connect
+	for {
+		con, err := sock.Accept()
+		if err != nil {
+			log.Println("Failed to accept: ", err)
+			continue;
+		}
+		go HandleClient(con)
 	}
 
 }
