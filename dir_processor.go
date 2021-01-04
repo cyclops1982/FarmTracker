72d29d5b3b0079650572f92ead812399dd3c0c36
Ruben d'Arco <cyclops@prof-x.net>
2021-01-04 21:21:40 +0000

    Add filename filtering.
    This introduces a super simple protocol where the client will need to tell the server
    what they want to receive. There's a few options:
    - up.
    - status.
    - join.
    - .
    
    Theres are currently the 3 types of messages that we get. The '.' will get you everything (no filter at all).

--- dir_processor.go
+++ dir_processor.go
@@ -1,3 +1,8 @@
+/*
+TODO:
+	- Provide a command line parameter to indicate how long back we should read.
+	  If nothing provided, then start reading from 'now'. This requires the FindFiles to understand date/time stuff.
+*/
 package main
 
 import (
@@ -10,9 +15,11 @@ import (
 	"fmt"
 	"encoding/json"
 	"io/ioutil"
+	"bytes"
+	"strings"
 )
 
-func FindFiles(ch chan string, path string) {
+func FindFiles(ch chan string, path string, filter string) {
 	foundDirs := make(map[string]bool)
 	for {
 		filepath.Walk(path, func(p string, info os.FileInfo, err error) error {
@@ -21,7 +28,7 @@ func FindFiles(ch chan string, path string) {
 				return err
 			}
 
-			if info.IsDir() == false {
+			if info.IsDir() == false && (filter == "" || strings.Contains(info.Name(), filter))  {
 				if foundDirs[p] == false {
 					ch <- p
 					foundDirs[p] = true
@@ -36,9 +43,44 @@ func FindFiles(ch chan string, path string) {
 
 func HandleClient(con net.Conn) {
 	defer con.Close()
-	ch := make(chan string)
-	go FindFiles(ch, inputdir)
 
+	// We want to understand a 'command' on what the filtering would be, or just a dot.
+	tmp := make([]byte, 30)
+	_, err := con.Read(tmp)
+	if err != nil {
+		log.Println("Failed to read from client",con.RemoteAddr(),". Disconnecting. Error: ", err)
+		return
+	}
+	// check if we have a dot, as that indicates the end of our command.
+	if bytes.ContainsRune(tmp, '.') == false {
+		con.Write([]byte("Sorry, i didn't get that. Bye."))
+		return
+	}
+
+	// Remove all chars, so we compare to a nice string.
+	replacer := strings.NewReplacer("\r", "", "\n", "", ".", "", "\x00", "")
+	filterinput := replacer.Replace(string(tmp[:]))
+
+	var filter string
+	switch filterinput {
+		case "up":
+			filter = "_up_"
+		case "status":
+			filter = "_status_"
+		case "join":
+			filter = "_join_"
+		default:
+			filter = ""
+	}
+	if filter != "" {
+		log.Println("Filtering returned items on ", filter)
+	}
+
+	// Now let's find some files in a seperate thread.
+	ch := make(chan string)
+	go FindFiles(ch, inputdir, filter)
+
+	// Read from the channel and send out the content.
 	for file := range ch {
 		bytes, err := ioutil.ReadFile(file)
 		if err != nil {
@@ -46,23 +88,25 @@ func HandleClient(con net.Conn) {
 			continue
 		}
 		// Verify that it's valid JSON.
-		// Possibly we'd do some filtering too.
 		var jsonData interface{}
 		err = json.Unmarshal(bytes, &jsonData)
 		if err != nil {
-			log.Println("Failed to parse JSON in ",file,". Error: ", err)
+			log.Printf("Failed to parse JSON. Ignoring file '%s'. Error: %s\n", file, err)
 			continue
 		}
+		/* 
+		Example of filtering:
 		realData := jsonData.(map[string]interface{})
 		if realData["applicationID"] != "1" {
 			log.Println("JSON payload is not for application 1. Skipping.")
 			continue
-		}
+		}*/
 		writenbytes, err := con.Write(bytes)
 		if err != nil {
-			log.Println("Failed to write bytes: ", err)
+			log.Println("Failed to write bytes. Disconnecting. Error was: ", err)
+			return
 		} else {
-			log.Println("Wrote ",writenbytes," to ", con.RemoteAddr())
+			log.Printf("Wrote %d bytes to %s.\n", writenbytes, con.RemoteAddr())
 		}
 	}
 }
@@ -75,18 +119,21 @@ func main() {
 	var ipAddress = flag.String("address", "0.0.0.0", "The IP address to bind to, default is 0.0.0.0.")
 	flag.Parse()
 
-	sock, err := net.Listen("tcp", fmt.Sprintf("%s:%d", *ipAddress, *tcpPort))
+	addr:=fmt.Sprintf("%s:%d", *ipAddress, *tcpPort)
+	sock, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatal("Couldn't open TCP socket.", err)
 	}
 
 	// Wait for clients to connect
+	log.Println("Listening on ", addr)
 	for {
 		con, err := sock.Accept()
 		if err != nil {
 			log.Println("Failed to accept: ", err)
 			continue;
 		}
+		log.Println("Client ",con.RemoteAddr()," connected. Starting Thread.")
 		go HandleClient(con)
 	}
 
