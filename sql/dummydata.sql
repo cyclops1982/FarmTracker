647255493c0e20a376700780dd1ec250b1d32d1c
Ruben d'Arco <cyclops@prof-x.net>
2021-01-05 22:34:58 +0000

    Simple dummy data and table definition

---
+++ sql/dummydata.sql
@@ -0,0 +1,2 @@
+INSERT INTO Device(Name, DeviceEUI) VALUES('My First Device','0004a30b00eb5e28')
+
--- sql/table_create.sql
+++ sql/table_create.sql
@@ -1,5 +1,15 @@
 /* Simple script to create the MySQL DB */
 
+CREATE TABLE Device(
+	Id INT NOT NULL AUTO_INCREMENT,
+	Name NVARCHAR(100) NOT NULL,
+	Description NVARCHAR(4086) NULL,
+	DeviceEUI NVARCHAR(64) NULL,
+	DevAddr NVARCHAR(32) NULL,
+	CONSTRAINT PK_Device PRIMARY KEY (Id),
+	CONSTRAINT UX_Device_Name UNIQUE KEY (Name)
+);
+
 CREATE TABLE Location(
 	DeviceId	INT NOT NULL,
 	LoggedOn	TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -7,7 +17,8 @@ CREATE TABLE Location(
 	Id		BIGINT NOT NULL AUTO_INCREMENT,
 	CONSTRAINT PK_Location PRIMARY KEY (Id),
 	CONSTRAINT UX_Location_LoggedOnDeviceId UNIQUE KEY (LoggedOn, DeviceId), /* Device can't be at different places at the same time */
+	CONSTRAINT FK_Location_Device FOREIGN KEY (DeviceId) REFERENCES Device(Id) ON DELETE NO ACTION ON UPDATE CASCADE,
 	INDEX IX_Location_When (LoggedOn) /* Make it fast to order by When. */
-)
+);
 
 
