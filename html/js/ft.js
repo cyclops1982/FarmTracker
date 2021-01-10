d9b0d9f90066974e1f2c34b1ba39ee2397dbb073
Ruben d'Arco <cyclops@prof-x.net>
2021-01-10 21:42:06 +0000

    WIP
    ^M is from vscode on windows.
    This is an initial implementation of /some/ stuff being shown.

--- html/js/ft.js
+++ html/js/ft.js
@@ -1,22 +1,83 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-   var mainmap = L.map("mainmap").setView([51.505, -0.09], 13);
-   /*
-  L.tileLayer(
-    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-    {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors.',
-      maxZoom: 18,
-      id: "openstreetmap",
-      tileSize: 256,
-      zoomOffset: -1,
-    }
-  ).addTo(mymap);*/
-
-   var layer1 = L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      subdomains: ["a", "b", "c"],
-   }).addTo(mainmap);
-
-   layers: [grayscale, cities];
+   var mainmap = LoadMap("mainmap");
 });
+
+function LoadMap(mapId) {
+   // Load data from JSON
+   var bla = {
+      Center: {
+         Lat: -30.8,
+         Long: 26.4,
+      },
+      Sheep: [
+         {
+            Id: 1,
+            Lat: -30.8,
+            Long: 26.417,
+            CreatedOn: 2010 - 02 - 23,
+         },
+         {
+            Id: 2,
+            Lat: -30.8012,
+            Long: 26.4188,
+            CreatedOn: 2010 - 02 - 26,
+         },
+      ],
+   };
+
+   var allSheeps = L.layerGroup();
+
+   bla.Sheep.forEach(function (item, index) {
+      var x = L.marker([item.Lat, item.Long]);
+      allSheeps.addLayer(x);
+   });
+
+   var objectsOnMap = {
+      Sheep: allSheeps,
+   };
+
+   var StreetMaps = L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
+      maxZoom: 19,
+   });
+
+   var OpenTopoMap = L.tileLayer("https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png", {
+      maxZoom: 17,
+      attribution:
+         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+   });
+
+   var Esri_WorldImagery = L.tileLayer(
+      "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+      {
+         attribution:
+            "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
+      }
+   );
+
+   var mainmap = L.map(mapId, {
+      layers: [Esri_WorldImagery, allSheeps],
+   });
+
+   L.control.scale().addTo(mainmap);
+
+   var layersVar = L.control
+      .layers(
+         {
+            Street: StreetMaps,
+            Topology: OpenTopoMap,
+            Satelite: Esri_WorldImagery,
+         },
+         {
+            Sheeps: allSheeps,
+         }
+      )
+
+      .addTo(mainmap)
+      .expand();
+
+   var mapCentre = L.latLng(bla.Center.Lat, bla.Center.Long);
+   mainmap.setView(mapCentre, 10);
+
+   return mainmap;
+}
--- readme.md
+++ readme.md
@@ -2,13 +2,14 @@
 
 FarmTracker is a project that helps farmers gain insight to their farm and assets on their farm.
 The initial goals are:
-- Sheep/Animal tracking
-- Water sensors
 
+-  Sheep/Animal tracking
+-  Water sensors
 
 ## Technical notes
 
 Go Version:
+
 ```
 $ go version
 go version go1.15.6 linux/amd64
@@ -17,15 +18,19 @@ go version go1.15.6 linux/amd64
 ## Architecture
 
 The following tools/apps exist:
-- http_receiver.go - This is a web service that receives a POST message from Chirpstack's http integration. Does validation if it is what we expect (a POST to a specific URL). Messages are written to disk in a specific structure that dir_processor.go reads.
-- dir_processor.go - Simple application that waits for something to connect, expects what to send out and then sends it. "Monitors" the filesystem for new files that are added. If file matches the filter, will then send it over.
-- base64parser.go - Simple utility to output the base64 data in readable format. Also a test to see if our base64 actually unpacks correctly.
-- location_logger.go - receives the 'up' messages from the dir_processor over the network and pushes it into the db.
+
+-  http_receiver.go - This is a web service that receives a POST message from Chirpstack's http integration. Does validation if it is what we expect (a POST to a specific URL). Messages are written to disk in a specific structure that dir_processor.go reads.
+-  dir_processor.go - Simple application that waits for something to connect, expects what to send out and then sends it. "Monitors" the filesystem for new files that are added. If file matches the filter, will then send it over.
+-  base64parser.go - Simple utility to output the base64 data in readable format. Also a test to see if our base64 actually unpacks correctly.
+-  location_logger.go - receives the 'up' messages from the dir_processor over the network and pushes it into the db.
 
 ## Installation/Running
 
-- Run the db_create.sql script. Please check the content.
-- Run the table_create.sql script.
-- Start the http_receiver.
-- Start the dir_processor.
+-  Run the db_create.sql script. Please check the content.
+-  Run the table_create.sql script.
+-  Start the http_receiver.
+-  Start the dir_processor.
 
+## Other notes
+
+-  Great way to demonstrate leaflet layers: https://leaflet-extras.github.io/leaflet-providers/preview/
