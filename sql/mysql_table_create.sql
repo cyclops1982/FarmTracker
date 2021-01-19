a3bdc55fa9adc1866c5d2e65a296b63abe9e8e0c
Ruben d'Arco <cyclops@prof-x.net>
2021-01-19 15:26:52 +0000

    Add battery status table

--- sql/mysql_table_create.sql
+++ sql/mysql_table_create.sql
@@ -1,6 +1,6 @@
 /* Simple script to create the MySQL DB */
 
-CREATE TABLE Device(
+CREATE TABLE Device (
 	Id INT NOT NULL AUTO_INCREMENT,
 	Name NVARCHAR(100) NOT NULL,
 	Description NVARCHAR(4086) NULL,
@@ -10,15 +10,25 @@ CREATE TABLE Device(
 	CONSTRAINT UX_Device_Name UNIQUE KEY (Name)
 );
 
-CREATE TABLE Location(
-	DeviceId	INT NOT NULL,
-	LoggedOn	TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	Location	POINT NOT NULL,
-	Id		BIGINT NOT NULL AUTO_INCREMENT,
+CREATE TABLE Location (
+	DeviceId INT NOT NULL,
+	LoggedOn TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	Location POINT NOT NULL,
+	Id BIGINT NOT NULL AUTO_INCREMENT,
 	CONSTRAINT PK_Location PRIMARY KEY (Id),
 	CONSTRAINT UX_Location_LoggedOnDeviceId UNIQUE KEY (LoggedOn, DeviceId), /* Device can't be at different places at the same time */
 	CONSTRAINT FK_Location_Device FOREIGN KEY (DeviceId) REFERENCES Device(Id) ON DELETE NO ACTION ON UPDATE CASCADE,
-	INDEX IX_Location_When (LoggedOn) /* Make it fast to order by When. */
+	INDEX IX_Location_LoggedOn (LoggedOn)
 );
 
-
+CREATE TABLE BatteryStatus (
+	Id BININT NOT NULL AUTO_INCREMENT,
+	DeviceId INT NOT NULL,
+	LoggedOn TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	RawValue INT NOT NULL,
+	BatteryLevel TINYINT NULL,
+	CONSTRAINT PK_BatteryStatus PRIMARY KEY (Id),
+	CONSTRAINT UX_BatteryStatus_LoggedOnDeviceId UNIQUE KEY (LoggedOn, DeviceId), /* A device can't reports it's battery value multiple times at the same time */
+	CONSTRAINT FK_BatteryStatus_Device FOREIGN KEY (DeviceId) REFERENCES (Device(Id ON DELETE NO ACTION ON UPDATE CASCADE,
+	INDEX IX_BatteryStatus_LoggedOn (LoggedOn)
+);
