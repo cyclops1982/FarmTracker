d00104fd98ddd3b8e78399eda779d989e587dad8
Ruben d'Arco <cyclops@prof-x.net>
2021-02-27 13:52:08 +0000

    Suggestions from #go-nuts - TIL io.ReadFull()

--- enhancedconn/enhancedconn.go
+++ enhancedconn/enhancedconn.go
@@ -12,56 +12,41 @@ type EnhancedConn struct {
 	net.Conn
 }
 
-func (con *EnhancedConn) ReadBytes(allBytes []byte, timeout time.Duration) (int, error) {
-	allReadBytes:=0
 
+func (con *EnhancedConn) ReadBytes(data []byte, timeout time.Duration) (int, error) {
+	allReadBytes := 0
 	if timeout != 0 {
 		con.SetReadDeadline(time.Now().Add(timeout * time.Second))
-	}	
-
-	for {
-		readBytes, err := con.Read(allBytes[allReadBytes:])
-		allReadBytes += readBytes
-		if err != nil {
-			log.Printf("Failed to read %d bytes: %v\n", len(allBytes), err)
-			return readBytes, err
-		}
-		if allReadBytes == len(allBytes) {
-			break
-		}
+		defer con.SetReadDeadline(time.Time{})
 	}
-	if timeout != 0 {
-		con.SetReadDeadline(time.Time{})
-	}
-	return allReadBytes, nil
+	return io.ReadFull(data)
 }
 
 
-func (con *EnhancedConn) ReadLength() uint16 {
-	bytes := make([]byte, 2)
-	length, err := con.ReadBytes(bytes, 0)
-	if (err != nil) {
-		log.Printf("Failed to read length: %v\n", err);
-		return 0
+func (con *EnhancedConn) ReadLength() (uint16, error) {
+	data := make([]byte, 2)
+	length, err := io.ReadFull(data)
+	if err != nil {
+		return 0, err
 	}
-	if length != len(bytes) {
-		log.Printf("Didn't read %d bytes, only got %d: %v\n", len(bytes), length, err)
-		return 0
+	if length != len(data) {
+		log.Printf("Didn't read %d bytes, only got %d.\n", len(data), length)
+		return 0, err
 	}
-
-	return binary.BigEndian.Uint16(bytes)
+	return binary.BigEndian.Uint16(bytes), nil
 }
 
 
-func (con *EnhancedConn) SendProtobufMsg(msg proto.Message) {
+func (con *EnhancedConn) SendProtobufMsg(msg proto.Message) error {
 	out, err := proto.Marshal(msg)
 	if err != nil {
-		log.Printf("Failed to Marshal protobuf message. Error: %v\n", err)
+		return err
 	}
-
-	// Send it over the network
 	lengthMsg := make([]byte, 2)
 	binary.BigEndian.PutUint16(lengthMsg, uint16(len(out)))
-	con.Write(lengthMsg)
-	con.Write(out)
+	if _, err := con.Write(lengthMsg); err != nil {
+		return err
+	}
+	_, err = con.Write(out)
+	return err
 }
